c148691adb898f0f729df629de03adf651bdbfe3
Astramast <azerqsdfazerqsdg@gmail.com>
2022-06-30 01:29:37 +0200

    Added Grid

---
+++ Grid.py
@@ -0,0 +1,15 @@
+class Grid:
+    def __init__(self, n, m=n):
+        self.height = n
+        self.width = m
+        self.matrix = [[0 for i in range(m)] for j in range(n)]
+
+    def getHeight(self):
+        return self.height
+
+    def getWidth(self):
+        return self.width
+
+    def getSquare(self, i, j):
+        return self.matrix[i][j]
+
